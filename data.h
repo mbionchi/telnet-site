d38de8ba5b1f84365e2838fe19f99a17ff5e8613
mbionchi <mbionchi@noreply.github.com>
2018-12-05 01:58:32 -0500

    separate out data-related functions

--- data.h
+++ data.h
@@ -18,6 +18,9 @@
 #ifndef _TELNET_SITE_DATA_H
 #define _TELNET_SITE_DATA_H
 
+#include <stdio.h>
+#include <dirent.h>
+
 struct line {
     char *line;
     struct line *prev, *next;
@@ -28,5 +31,12 @@ struct section {
     char *filename;
     struct line *content;
 };
+
+void print_lines(struct line *lines);
+void dump_sections(struct section **sections, size_t n_sections);
+struct line *read_lines(FILE *fp);
+struct section **read_sections(DIR *dir, char *dirname, int follow_links, size_t *nmemb);
+struct line *flow_content(struct line *lines, int width);
+void free_lines(struct line *lines);
 
 #endif
