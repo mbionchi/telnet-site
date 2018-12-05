02bce60b2316a9a7146db9f9c08215fdb1e60add
mbionchi <mbionchi@noreply.github.com>
2018-12-05 01:51:58 -0500

    again forgot to add license + fix preamble

--- site.c
+++ site.c
@@ -12,7 +12,7 @@
  *   GNU General Public License for more details.
  *
  *   You should have received a copy of the GNU General Public License
- *   along with untrace.  If not, see <https://www.gnu.org/licenses/>.
+ *   along with telnet-site.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "render.h"
